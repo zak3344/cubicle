b7e15011e6d0c083f8f1bbfc99cf5a2685abb0e4
Vasil <vasilignatow@gmail.com>
2022-06-12 18:33:25 +0300

    Add images and styles

---
+++ src/config/handlebars.js
@@ -0,0 +1,14 @@
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+console.log( handlebars)
+
+const initHandlebars = (app) => {
+    app.set('views', path.resolve(__dirname, '../views'));
+    app.engine('hbs', handlebars({
+        extname: 'hbs'
+    }));
+    app.set('view engine', 'hbs')
+};
+
+module.exports = initHandlebars;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 const port = 5000;
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+const initHandlebars = require('./config/handlebars');
 
 const app = express();
 
-app.set('views', path.resolve(__dirname));
-app.engine('hbs', handlebars({
-    extname: 'hbs'
-}));
-app.set('view engine', 'hbs')
+initHandlebars(app);
 
 app.all('/', (req, res) => {
     res.render('index', { layout: false});
-    res.end();
-})
+});
 
 
 app.listen(port, console.log.bind(console, `App is running on port http://localhost:${port}`));
