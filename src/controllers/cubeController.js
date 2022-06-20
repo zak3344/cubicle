82de8d720efbbf30011626db24d0ec379043f7fa
Vasil <vasilignatow@gmail.com>
2022-06-20 15:51:42 +0300

    Add Details page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -7,19 +7,25 @@ const router = express.Router();
 const renderCreatePage = (req, res) => {
     let cubes = cubeService.getAll();
     console.log(cubes);
-    
+
     res.render('create');
 };
 
 const createCube = (req, res) => {
     let { name, description, imageUrl, difficulty } = req.body;
-    
+
     cubeService.create(name, description, imageUrl, difficulty);
 
     res.redirect('/cube/create');
 }
 
+const cubeDetails = (req, res) => {
+    let cube = cubeService.getOne(req.params.cubeId);
+    res.render({ ...cube });
+}
+
 router.get('/create', renderCreatePage);
 router.post('/create', createCube);
+router.get('/:cubeId', cubeDetails);
 
 module.exports = router;
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,9 +1,12 @@
 const Cube = require('../modles/Cube');
-
+console.log(Cube.cubes);
 
 const cubeDb = [];
 
 const getAll = () => Cube.cubes;
+
+const getOne = (id) => Cube.cubes.find(c => c.id == id);
+
 
 const create = (name, descriptionm, imageUrl, difficulty) => {
     let cube = new Cube(name, descriptionm, imageUrl, difficulty);
@@ -11,8 +14,11 @@ const create = (name, descriptionm, imageUrl, difficulty) => {
 };
 
 
+
+
 const cubeService = {
     getAll,
+    getOne,
     create
 }
 
