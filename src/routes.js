56ef318e239927134c0c503ff7a7a68401b1523e
Vasil <vasilignatow@gmail.com>
2022-08-06 18:00:29 +0300

    Create Auth Controller

--- src/routes.js
+++ src/routes.js
@@ -3,14 +3,16 @@ const express = require('express');
 const cubeController = require('./controllers/cubeController');
 const homeController = require('./controllers/homeConteroller');
 const accessoryController = require('./controllers/accessoryController');
+const authController = require('./controllers/authController');
 
 const router = express.Router();
 
 router.use(homeController);
 router.use('/cube', cubeController);  //Respond to /cube/create route
 router.use('/accessory', accessoryController); // Respond to /accessory/create
+router.use(authController);
 router.use('*', (req, res) => {
-    res.render('404');
+    res.status(404).render('404');
 });
 
 
