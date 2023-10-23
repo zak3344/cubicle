6e31c50326a1cf7992665495c50f568ce128820a
vasilignatov <76693214+vasilignatov@users.noreply.github.com>
2023-10-23 15:34:39 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,17 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000,
+        PORT: process.env.PORT || 3000,
         DB_CONNECTION_STRING: 'mongodb://localhost:27017/cubes',
         JWT_SECRET: '$2y$10$xzlJAyCJAfoaBZIez.I0EO3KZAuXr66Voh/YmIQrGnjKUN3/WOJSq',
         TOKEN_COOKIE_NAME: 'cubicale_token'
     },
     production: {
-        port: 5000,
-        DB_CONNECTION_STRING: 'your_atlas_connection_string',
-        JWT_SECRET: '$2y$10$xzlJAyCJAfoaBZIez.I0EO3KZAuXr66Voh/YmIQrGnjHJK3/WOJSq',
-        TOKEN_COOKIE_NAME: 'cubicale_token'
+        PORT: process.env.PORT,
+        DB_CONNECTION_STRING: process.env.DB_CONNECTION_STRING,
+        JWT_SECRET: process.env.JWT_SECRET,
+        TOKEN_COOKIE_NAME: process.env.TOKEN_COOKIE_NAME
     }
 };
+// '$2y$10$xzlJAyCJAfoaBZIez.I0EO3KZAuXr66Voh/YmIQrGnjHJK3/WOJSq'
+
+// 'cubicale_token'
